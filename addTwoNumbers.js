eb82a129ae2e4ea8f6173b64de60d76eac0383a5
diBasch <barbash.dv@gmail.com>
2021-11-14 19:05:08 +0300

    addTwoNumbers 2

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -14,4 +14,20 @@ const addTwoNumbers = (l1, l2) => {
     .map((e) => parseInt(e));
 };
 
+const addTwoNumbers2 = (ary1, ary2) => {
+  let result;
+  const revers = (array) => {
+    const l = array.length;
+    let rev = "";
+    for (let i = l - 1; i >= 0; i--) {
+      rev = rev + array[i];
+    }
+    return rev;
+  };
+  result = revers((revers(ary1) * 1 + revers(ary2) * 1).toString());
+  return result.split("").map((e) => e * 1);
+};
+
 console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
+
+console.log(addTwoNumbers2([2, 4, 3], [5, 6, 4]));
---
+++ lengthOfLongestSubstring.js
@@ -0,0 +1,9 @@
+// Longest Substring Without Repeating Characters
+
+// Given a string s, find the length of the longest substring without repeating characters.
+
+const lengthOfLongestSubstring = (s) => {
+  return s;
+};
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
